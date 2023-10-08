ffcc87607c85cc67374f715e78fe443e772a4543
AndreyPushkinA <57715810+AndreyPushkinA@users.noreply.github.com>
2023-10-08 16:47:06 +0200

    Update clickhouse.sh

--- clickhouse-client/clickhouse.sh
+++ clickhouse-client/clickhouse.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo "CREATE DATABASE IF NOT EXISTS clickhouse ENGINE = MaterializedPostgreSQL('192.168.100.237:5432', 'homeassistant', 'homeassistant', 'homeassistant');" | clickhouse-client --host 192.168.100.237 --port 9000
+get_option_value() {
+        grep -A 8 "\"options\"" config.json | grep -m 1 "\"$1\"" | cut -d ':' -f2 | tr -d ' ",'
+}
+
+DB_NAME=$(get_option_value "db_name")
+PG_HOST=$(get_option_value "pg_host")
+PG_PORT=$(get_option_value "pg_port")
+PG_DB=$(get_option_value "pg_db")
+PG_USER=$(get_option_value "pg_user")
+PG_PASS=$(get_option_value "pg_pass")
+CH_HOST=$(get_option_value "ch_host")
+CH_PORT=$(get_option_value "ch_port")
+
+echo "CREATE DATABASE IF NOT EXISTS ${DB_NAME} ENGINE = MaterializedPostgreSQL('${PG_HOST}:${PG_PORT}', '${PG_DB}', '${PG_USER}', '${PG_PASS}');" | clickhouse-client --host ${CH_HOST} --port ${CH_PORT}
