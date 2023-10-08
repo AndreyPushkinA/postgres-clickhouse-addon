e0b73b61d52c10a3e4773a2ce80575e5f9f23c57
AndreyPushkinA <57715810+AndreyPushkinA@users.noreply.github.com>
2023-10-08 12:53:57 +0200

    Update clickhouse.sh

--- clickhouse-client/clickhouse.sh
+++ clickhouse-client/clickhouse.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-clickhouse-client --host 192.168.100.237 --port 9001
-
-echo "SET allow_experimental_database_materialized_postgresql=1;" | clickhouse-client --host 192.168.100.237 --port 9001
+clickhouse-client --host 192.168.100.237 --port 9001 --query="SET allow_experimental_database_materialized_postgresql=1;"
 
 # echo "CREATE DATABASE IF NOT EXISTS clickhouse_db ENGINE = MaterializedPostgreSQL('192.168.100.237:5432', 'homeassistant', 'homeassistant', 'homeassistant');"
