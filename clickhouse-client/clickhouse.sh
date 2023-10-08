9ba6c60b40d28f0226a68cb545345f33e1f26b8a
AndreyPushkinA <57715810+AndreyPushkinA@users.noreply.github.com>
2023-10-08 16:58:40 +0200

    Update clickhouse.sh

--- clickhouse-client/clickhouse.sh
+++ clickhouse-client/clickhouse.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-CONFIG_PATH=/data/options.json
+CONFIG_PATH="/data/options.json"
 
-DB_NAME="$(bashio::config 'db_name')"
-PG_HOST="$(bashio::config 'pg_host')"
-PG_DB="$(bashio::config 'pg_db')"
-PG_USER="$(bashio::config 'pg_user')"
-PG_PASS="$(bashio::config 'pg_pass')"
-CH_HOST="$(bashio::config 'ch_host')"
-CH_PORT="$(bashio::config 'ch_port')"
+get_option_value() {
+        grep -A 8 "\"options\"" $CONFIG_PATH | grep -m 1 "\"$1\"" | cut -d ':' -f2 | tr -d ' ",'
+}
+
+DB_NAME=$(get_option_value "db_name")
+PG_HOST=$(get_option_value "pg_host")
+PG_PORT=$(get_option_value "pg_port")
+PG_DB=$(get_option_value "pg_db")
+PG_USER=$(get_option_value "pg_user")
+PG_PASS=$(get_option_value "pg_pass")
+CH_HOST=$(get_option_value "ch_host")
+CH_PORT=$(get_option_value "ch_port")
 
 echo "CREATE DATABASE IF NOT EXISTS ${DB_NAME} ENGINE = MaterializedPostgreSQL('${PG_HOST}:${PG_PORT}', '${PG_DB}', '${PG_USER}', '${PG_PASS}');" | clickhouse-client --host ${CH_HOST} --port ${CH_PORT}
