9edf5580f759044353d6c8211bf9413d24e0dd30
AndreyPushkinA <57715810+AndreyPushkinA@users.noreply.github.com>
2023-10-09 21:39:41 +0200

    Update run.sh

--- clickhouse-client/run.sh
+++ clickhouse-client/run.sh
@@ -15,13 +15,4 @@ PG_PASS=$(get_option_value "pg_pass")
 CH_HOST=$(get_option_value "ch_host")
 CH_PORT=$(get_option_value "ch_port")
 
-echo "DB_NAME: $DB_NAME"
-echo "PG_HOST: $PG_HOST"
-echo "PG_PORT: $PG_PORT"
-echo "PG_DB: $PG_DB"
-echo "PG_USER: $PG_USER"
-echo "PG_PASS: $PG_PASS"
-echo "CH_HOST: $CH_HOST"
-echo "CH_PORT: $CH_PORT"
-
 echo "CREATE DATABASE IF NOT EXISTS ${DB_NAME} ENGINE = MaterializedPostgreSQL('${PG_HOST}:${PG_PORT}', '${PG_DB}', '${PG_USER}', '${PG_PASS}');" | clickhouse-client --host ${CH_HOST} --port ${CH_PORT}
